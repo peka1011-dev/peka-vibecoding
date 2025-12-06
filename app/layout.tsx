7d372d78b3be7cb9eda8dc82e6fefd026604b83e
peka1011-dev <shxodbs1011@gmail.com>
2025-12-06 14:43:49 +0900

    Add AdSense verification script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8123007670953472"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
